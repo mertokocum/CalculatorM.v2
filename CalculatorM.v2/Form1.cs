5e769d187f03a39d8f1603feedb3b197be7d5e63
mertokocum <hmrtkocum@gmail.com>
2024-03-22 14:50:07 +0300

    Fix Reset label with C button. variable names changed.

--- CalculatorM.v2/Form1.cs
+++ CalculatorM.v2/Form1.cs
@@ -5,22 +5,21 @@ namespace CalculatorM.v2
     public partial class Form1 : Form
     {
 
-        Double result = 0;
-        String operatorSelected = " ";
-        bool isOperatorSelected = false;
+        double _result = 0;
+        string _operatorSelected = " ";
+        bool _isOperatorSelected = false;
         public Form1()
         {
             InitializeComponent();
-
         }
 
         private void button_click(object sender, EventArgs e)
         {
-            if ((textBoxResult.Text == "0") || (isOperatorSelected))
+            if ((textBoxResult.Text == "0") || (_isOperatorSelected))
             {
                 textBoxResult.Clear();
             }
-            isOperatorSelected = false;
+            _isOperatorSelected = false;
             Button clickedButton = sender as Button;
 
             if (clickedButton.Text == ",")  // block repetition of , 
@@ -29,7 +28,6 @@ namespace CalculatorM.v2
                 {
                     textBoxResult.Text += clickedButton.Text;
                 }
-
             }
             else textBoxResult.Text += clickedButton.Text;
 
@@ -41,65 +39,59 @@ namespace CalculatorM.v2
         {
             Button clickedButton = sender as Button;
 
-            if (result != 0) // if  result is not 0 clicking the operator performs the button_equals
+            if (_result != 0) // if  result is not 0 clicking the operator performs the button_equals
             {
                 buttonEquals.PerformClick();
-                operatorSelected = clickedButton.Text;
-                label_selectedOperator.Text = result + " " + operatorSelected;
-                isOperatorSelected = true;
+                _operatorSelected = clickedButton.Text;
+                label_selectedOperator.Text = _result + " " + _operatorSelected;
+                _isOperatorSelected = true;
             }
             else
             {
-                operatorSelected = clickedButton.Text;
-                result = Double.Parse(textBoxResult.Text); //String to double
-                label_selectedOperator.Text = result + " " + operatorSelected;
-                isOperatorSelected = true;
+                _operatorSelected = clickedButton.Text;
+                _result = double.Parse(textBoxResult.Text); //String to double
+                label_selectedOperator.Text = _result + " " + _operatorSelected;
+                _isOperatorSelected = true;
             }
         }
 
         private void buttonCE_click(object sender, EventArgs e)
         {
             textBoxResult.Text = "0";
-
         }
 
         private void buttonC_click(object sender, EventArgs e)
         {
             textBoxResult.Text = "0";
-            result = 0;
-
+            _result = 0;
+            label_selectedOperator.Text = "";
         }
 
         private void buttonEquals_Click(object sender, EventArgs e)
         {
-            string expression = label_selectedOperator.Text;
-            double resultValue = result;
-
-
-            switch (operatorSelected)
+            switch (_operatorSelected)
             {
-
                 case "+":
-                    textBoxResult.Text = (result + Double.Parse(textBoxResult.Text)).ToString();
+                    textBoxResult.Text = (_result + Double.Parse(textBoxResult.Text)).ToString();
                     break;
 
                 case "-":
-                    textBoxResult.Text = (result - Double.Parse(textBoxResult.Text)).ToString();
+                    textBoxResult.Text = (_result - Double.Parse(textBoxResult.Text)).ToString();
                     break;
 
                 case "X":
-                    textBoxResult.Text = (result * Double.Parse(textBoxResult.Text)).ToString();
+                    textBoxResult.Text = (_result * Double.Parse(textBoxResult.Text)).ToString();
                     break;
 
                 case "/":
-                    textBoxResult.Text = (result / Double.Parse(textBoxResult.Text)).ToString();
+                    textBoxResult.Text = (_result / Double.Parse(textBoxResult.Text)).ToString();
                     break;
 
                 default:
                     break;
             }
 
-            result = Double.Parse(textBoxResult.Text);
+            _result = double.Parse(textBoxResult.Text);
             label_selectedOperator.Text = "";
         }
 
